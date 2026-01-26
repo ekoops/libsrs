c3a1087e0b3e83ead5613db35e5781ecd86ae416
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2026-01-26 11:38:37 +0100

    feat(read): add `readlink()` helper
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- src/read.rs
+++ src/read.rs
@@ -1,3 +1,5 @@
+use libc::c_char;
+use std::ffi::CStr;
 use std::fs::File;
 use std::io;
 use std::io::Read;
@@ -18,4 +20,20 @@ pub fn read_exact(file: &mut File, buff: &mut [u8]) -> io::Result<usize> {
         }
     }
     Ok(read_bytes)
+}
+
+/// Read the content of the symbolic link `path` into `buff`. Return the amount of data read.
+pub fn readlink(path: &CStr, buff: &mut [u8]) -> io::Result<usize> {
+    let ret = unsafe {
+        libc::readlink(
+            path.as_ptr() as *const c_char,
+            buff.as_ptr() as *mut c_char,
+            buff.len(),
+        )
+    };
+    if ret < 0 {
+        return Err(io::Error::last_os_error());
+    }
+
+    Ok(ret as usize)
 }
