c072737f282fdfa9d65faf677d0821b7459b401c
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2026-01-27 21:21:37 +0100

    feat: add `CappedOsString` for `OsString`s with a max length
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

---
+++ src/capped_os_string.rs
@@ -0,0 +1,29 @@
+use std::ffi::{OsStr, OsString};
+use std::io;
+use std::ops::Deref;
+use std::os::unix::ffi::OsStrExt;
+use crate::buffer_writer::{BufferWriter, FromBufferWriter};
+
+/// An [OsString] with a maximum length equal to [MAX_LEN].
+#[derive(Clone, Default)]
+pub struct CappedOsString<const MAX_LEN: usize>(OsString);
+
+impl<const MAX_LEN: usize> FromBufferWriter for CappedOsString<MAX_LEN> {
+    #[inline]
+    fn from_buffer_writer<W: BufferWriter>(writer: W) -> io::Result<Self> {
+        let mut buff = [0u8; MAX_LEN];
+        let written_bytes = writer.write(&mut buff)?;
+        // Cap written bytes to be sure it is not bigger than buffer size.
+        let written_bytes = std::cmp::min(written_bytes, buff.len());
+        let os_str = OsStr::from_bytes(&buff[..written_bytes]);
+        Ok(Self(OsString::from(os_str)))
+    }
+}
+
+impl<const MAX_LEN: usize> Deref for CappedOsString<MAX_LEN> {
+    type Target = OsString;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@ pub mod read;
 pub mod task;
 pub mod procfs;
 pub mod buffer_writer;
+pub mod capped_os_string;
