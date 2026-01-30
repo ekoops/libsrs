cfa0d9f40eb3a13a1d106438d0b1d5255b932410
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2026-01-30 17:33:27 +0100

    chore: allow to specify buffer elements type in `{From,}BufferWriter`
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- src/buffer_writer.rs
+++ src/buffer_writer.rs
@@ -1,23 +1,24 @@
 use std::io;
 
-/// An abstraction for types that can write data into a provided mutable buffer.
-pub trait BufferWriter {
-    /// Write data into `buff` and return the number of bytes written.
-    fn write(self, buff: &mut [u8]) -> io::Result<usize>;
+/// An abstraction for types that can write elements of type [T] into a provided mutable buffer.
+pub trait BufferWriter<T> {
+    /// Write data into `buff` and return the number of elements written.
+    fn write(self, buff: &mut [T]) -> io::Result<usize>;
 }
 
 /// Blanket implementation allowing the use of closures as [BufferWriter].
-impl<F> BufferWriter for F
+impl<T, F> BufferWriter<T> for F
 where
-    F: FnOnce(&mut [u8]) -> io::Result<usize>,
+    F: FnOnce(&mut [T]) -> io::Result<usize>,
 {
-    fn write(self, buff: &mut [u8]) -> io::Result<usize> {
+    fn write(self, buff: &mut [T]) -> io::Result<usize> {
         self(buff)
     }
 }
 
-/// A trait for types that can be constructed by providing a scratch buffer to a writer.
-pub trait FromBufferWriter: Sized {
+/// A trait for types that can be constructed by providing a scratch buffer of elements of type [T]
+/// to a writer.
+pub trait FromBufferWriter<T>: Sized {
     /// Construct an instance of [Self] by leveraging `writer`.
-    fn from_buffer_writer<W: BufferWriter>(writer: W) -> io::Result<Self>;
+    fn from_buffer_writer<W: BufferWriter<T>>(writer: W) -> io::Result<Self>;
 }
--- src/capped_os_string.rs
+++ src/capped_os_string.rs
@@ -1,16 +1,16 @@
+use crate::buffer_writer::{BufferWriter, FromBufferWriter};
 use std::ffi::{OsStr, OsString};
 use std::io;
 use std::ops::Deref;
 use std::os::unix::ffi::OsStrExt;
-use crate::buffer_writer::{BufferWriter, FromBufferWriter};
 
 /// An [OsString] with a maximum length equal to [MAX_LEN].
 #[derive(Clone, Default)]
 pub struct CappedOsString<const MAX_LEN: usize>(OsString);
 
-impl<const MAX_LEN: usize> FromBufferWriter for CappedOsString<MAX_LEN> {
+impl<const MAX_LEN: usize> FromBufferWriter<u8> for CappedOsString<MAX_LEN> {
     #[inline]
-    fn from_buffer_writer<W: BufferWriter>(writer: W) -> io::Result<Self> {
+    fn from_buffer_writer<W: BufferWriter<u8>>(writer: W) -> io::Result<Self> {
         let mut buff = [0u8; MAX_LEN];
         let written_bytes = writer.write(&mut buff)?;
         // Cap written bytes to be sure it is not bigger than buffer size.
