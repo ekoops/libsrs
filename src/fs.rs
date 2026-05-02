0d055fa51ccc3fb3f4eec64803b7475a0210e640
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2026-05-02 12:01:04 +0200

    chore(fs): add `DirEntry` abstraction and `scan_dir()` API
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- src/fs.rs
+++ src/fs.rs
@@ -1,6 +1,19 @@
-use rustix::fs::{self, Stat};
+use rustix::fs::{self, Dir, Mode, OFlags, Stat, CWD};
 use std::ffi::CStr;
 use std::io;
+use std::os::fd::{AsFd, BorrowedFd, OwnedFd};
+
+/// Invokes `openat(2)` system call.
+///
+/// Arguments have the same semantic of the underlying system call. Returns the open file
+/// descriptor.
+///
+/// # Errors
+///
+/// Returns an [`io::Error`] (sourced from errno) if the underlying `openat(2)` system call fails.
+fn openat<Fd: AsFd>(fd: Fd, path: &CStr, flags: OFlags, mode: Mode) -> io::Result<OwnedFd> {
+    fs::openat(fd, path, flags, mode).map_err(Into::into)
+}
 
 /// File metadata.
 pub struct Metadata(Stat);
@@ -21,4 +34,40 @@ impl MetadataExt for Metadata {
 pub fn read_metadata(path: &CStr) -> io::Result<Metadata> {
     let stat = fs::stat(path)?;
     Ok(Metadata(stat))
+}
+
+/// Directory entry.
+pub struct DirEntry<'a> {
+    fd: BorrowedFd<'a>,
+    entry: fs::DirEntry,
+}
+
+/// Iterates over the entries of the directory at `path`, executing `process` for each entry.
+///
+/// Entries for the current and parent directories (typically `.` and `..`) are skipped.
+///
+/// The `process` closure can return an [io::Result] to handle errors or abort the iteration early.
+pub fn scan_dir<P>(path: &CStr, mut process: P) -> io::Result<()>
+where
+    P: FnMut(&DirEntry<'_>) -> io::Result<()>,
+{
+    let dir_fd = openat(
+        CWD,
+        path,
+        OFlags::RDONLY | OFlags::DIRECTORY | OFlags::CLOEXEC,
+        Mode::empty(),
+    )?;
+    for dir_entry in Dir::read_from(dir_fd.as_fd())? {
+        let dir_entry = dir_entry?;
+        let name_bytes = dir_entry.file_name().to_bytes();
+        if name_bytes == b"." || name_bytes == b".." {
+            continue;
+        }
+        let dir_entry = DirEntry {
+            fd: dir_fd.as_fd(),
+            entry: dir_entry,
+        };
+        process(&dir_entry)?;
+    }
+    Ok(())
 }
