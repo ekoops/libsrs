e11cd3f7a381b019aac320ba11e3c4a8936b8786
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2026-05-20 14:08:45 +0200

    chore(buffer_writer): add `BufferWriter<T>::write_next_to_buff()`
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- src/buffer_writer.rs
+++ src/buffer_writer.rs
@@ -1,9 +1,26 @@
-use std::io;
+use std::{io, mem};
+
+/// Updates `*buff` to point to `n`-th element from the current position.
+fn advance_buff<T>(buff: &mut &mut [T], n: usize) {
+    let (_, new_buff) = mem::take(buff).split_at_mut(n);
+    *buff = new_buff;
+}
 
 /// An abstraction for types that can write elements of type `[T]` into a provided mutable buffer.
 pub trait BufferWriter<T> {
     /// Writes data into `buff` and returns the number of elements written.
     fn write_to_buff(self, buff: &mut [T]) -> io::Result<usize>;
+
+    /// Writes data into `buff` and returns the number of elements written.  Updates `*buff` to
+    /// point to the first unwritten element.
+    fn write_next_to_buff(self, buff: &mut &mut [T]) -> io::Result<usize>
+    where
+        Self: Sized,
+    {
+        let written_elems = self.write_to_buff(buff)?;
+        advance_buff(buff, written_elems);
+        Ok(written_elems)
+    }
 }
 
 /// Blanket implementation allowing the use of closures as [BufferWriter].
